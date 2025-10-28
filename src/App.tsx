3acaf5cd6194a6f6b538d8bd5152f1f863cabce5
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2025-10-28 17:12:55 -0500

    feta:add-navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar'
 import BpoHappifySection from './components/BpoHappifySection'
 import ServicesSection from './components/ServicesSection'
 import HeroSection from './components/HeroSection'
@@ -5,6 +6,7 @@ import HeroSection from './components/HeroSection'
 function App() {
   return (
     <div className="overflow-x-hidden">
+      <Navbar />
       <div className="bg-white">
         <div className="bg-gradient-to-br from-[#9b6cf8] to-[#b89dfa] rounded-b-[80px] md:rounded-b-[6%]">
           <HeroSection />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="w-full text-white text-center py-2 px-6 md:py-4" style={{ backgroundColor: '#B590FF' }}>
+            <p className="text-lg opacity-90">
+                Lorem ipsum dolor sit amet, consectetur adipiscing <span className="font-semibold" style={{ color: '#004AAD' }}>elit</span>.
+            </p>
+        </nav>
+    );
+}
