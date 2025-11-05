12b2aeb43c5e8b5e86998515c40097729b877044
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2025-11-05 11:15:04 -0500

    fix:add-To-home-Pwa

--- vite.config.ts
+++ vite.config.ts
@@ -9,25 +9,9 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["Frame 38.png"],
-      manifest: {
-        name: "Hilda Palacios - BPO Happify",
-        short_name: "BPO Happify",
-        description: "Servicios de BPO y Contact Center",
-        theme_color: "#004AAD",
-        background_color: "#ffffff",
-        display: "standalone",
-        icons: [
-          {
-            src: "/Frame 38.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/Frame 38.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+      manifest: false,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,json}"],
       },
     }),
   ],
