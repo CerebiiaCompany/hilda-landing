f9b693ffcfcb7a364d2bd5d0fc7d47acd4a33ca1
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2025-11-05 11:08:20 -0500

    fix:add-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["Frame 38.png"],
+      manifest: {
+        name: "Hilda Palacios - BPO Happify",
+        short_name: "BPO Happify",
+        description: "Servicios de BPO y Contact Center",
+        theme_color: "#004AAD",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/Frame 38.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/Frame 38.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/",
 });
